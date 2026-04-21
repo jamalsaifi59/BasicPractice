0bdc60847dc77aa7d1e189fdab86081c8cf6f50e
jamalsaifi59 <jamalsaifi379@gmail.com>
2026-04-21 13:14:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// const accountId = "2354145";
+// let accountName = "jamal saifi";
+// var accountCity = "Najibabad"
+
+// console.log(accountCity)
+// console.table([accountId, accountCity, accountName])
+
+//Not allow to this type of code
+accountCity = "Najibabad";
+accountId = "123456";
+accountName = "jamal saifi"
+
+// console.table([accountCity, accountId, accountName])
+
+let age = "35jamal";
+// console.log (typeof age)
+
+let valueInNumber = Number(age)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+/*
+type of conversion
+"26" => 26
+"26jamal" => NaN(Not a Number)
+true => 1, 
+false => 0
+*/
+
+let  inLoggedIn = 1;
+let booleanLogIn = Boolean(inLoggedIn)
+console.log(booleanLogIn)
+/*
+ 1 => true
+ "" => false
+ "jamal" => true
+*/
