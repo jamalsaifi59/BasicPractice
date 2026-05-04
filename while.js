8842ceb312b19e8b2dd81754ecb5c85c7a94d2dd
jamalsaifi59 <jamalsaifi379@gmail.com>
2026-05-04 12:44:05 +0530

    loops

---
+++ while.js
@@ -0,0 +1,22 @@
+
+// While and Do while loops
+
+let index = 0
+while (index <= 10) {
+    // console.log(`value of index ${index}`)
+    index = index + 1
+}
+
+let myArray = ["flash", "batman", "superman"]
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`value is ${myArray[arr]}`)
+    arr = arr +1
+}
+
+
+let score = 1
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score <= 10);
